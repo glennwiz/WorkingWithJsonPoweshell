73e9d0c4803cda71497e41d866d0d4e337023986
Glenn <glennwiz@gmail.com>
2021-12-23 16:30:11 +0100

    changed the names

--- RunTest.ps1
+++ RunTest.ps1
@@ -10,24 +10,24 @@ $inputFile  = 'C:\dev\PSJsonTesting\newItems.json'  # JSON fila som skal lastes 
 $updateFile = 'C:\dev\PSJsonTesting\test.json'      # JSON fila som skal oppdateres
 
 #### 2. convert the Json-formatted strings to Json-objects so you can start working with this ONLY in memory
-$inputJson = Get-Content -Path $inputFile -encoding utf8 | ConvertFrom-Json # Laster inn inputfila i minne
-$updateJson = Get-Content -Path $updateFile -encoding utf8 | ConvertFrom-Json  # Laster inn updatefila i minne 
+$inputObj = Get-Content -Path $inputFile -encoding utf8 | ConvertFrom-Json # Laster inn inputfila i minne
+$outputObj = Get-Content -Path $updateFile -encoding utf8 | ConvertFrom-Json  # Laster inn updatefila i minne to a 
 
 #### 3. modify the Json-object
-# loop through all items in the inputJson.listeMedUsers array
-# and add them to the updateJson.listeMedUsers array if they do not exist in the updateJson.listeMedUsers array
-foreach ($user in $inputJson.listeMedUsers) 
+# loop through all items in the inputObj$inputObj.listeMedUsers array
+# and add them to the outputObj$outputObj.listeMedUsers array if they do not exist in the outputObj$outputObj.listeMedUsers array
+foreach ($user in $inputObj.listeMedUsers) 
 {
-    if (!($user.Name -in $updateJson.listeMedUsers.Name)) #if NOT in array add item
+    if (!($user.Name -in $outputObj.listeMedUsers.Name)) #if NOT in array add item
     {
-        Write-Host "Adding $user.Name to $updateJson.listeMedUsers"
-        $updateJson.listeMedUsers += $user
+        Write-Host "Adding $user.Name to $outputObj.listeMedUsers"
+        $outputObj.listeMedUsers += $user
     }
 }
 
 #### 4. covert the Json-object to a Json-formatted string
-$updateJson = ConvertTo-Json -InputObject $updateJson # convert the memory Json-object to a Json-formatted memory string
+$outputObj = ConvertTo-Json -InputObject $outputObj # convert the memory Json-object to a Json-formatted memory string
 
 #### 5. save the result Json-Object string back to the SAME file overwriting it with new data set
-$updateJson | Out-File -Path $updateFile -Encoding UTF8  # save the Json-formatted string to the file overwriting it with new data
+$outputObj | Out-File -Path $updateFile -Encoding UTF8  # save the Json-formatted string to the file overwriting it with new data
 
