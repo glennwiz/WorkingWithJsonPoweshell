6ca4b88e82bb5aecd7d0591d2bc38ab7c565dc0e
Glenn <glennwiz@gmail.com>
2021-12-23 15:41:22 +0100

    Working with json files powershell

---
+++ RunTest.ps1
@@ -0,0 +1,35 @@
+#1 read a Json-formatted string from a file
+#2 convert the Json-formatted string to a Json-object
+#3 modify the Json-object
+#4 covert the Json-object to a Json-formatted string
+#5 save the Json-formatted string to a file
+
+
+#### 1. read a Json-formatted string from a file
+$inputFile  = 'C:\dev\PSJsonTesting\newItems.json'  # JSON fila som skal lastes inn med nye items
+$updateFile = 'C:\dev\PSJsonTesting\test.json'      # JSON fila som skal oppdateres
+
+#### 2. convert the Json-formatted strings to Json-objects so you can start working with this ONLY in memory
+$inputJson = Get-Content -Path $inputFile -encoding utf8 | ConvertFrom-Json # Laster inn inputfila i minne
+$updateJson = Get-Content -Path $updateFile -encoding utf8 | ConvertFrom-Json  # Laster inn updatefila i minne
+
+#### 3. modify the Json-object
+#loop through all items in the inputJson.listeMedUsers file and add them to the updateJson.listeMedUsers file if they do not exist in the updateJson.listeMedUsers file
+foreach ($item in $inputJson.listeMedUsers) {
+    if (!($item.Name -in $updateJson.listeMedUsers.Name)) {
+        Write-Host "Adding $item.Name to $updateJson.listeMedUsers"
+        $updateJson.listeMedUsers += $item
+    }
+}
+
+
+Write-Host $test.User
+
+
+
+#### 4. covert the Json-object to a Json-formatted string
+$updateJson = ConvertTo-Json -InputObject $updateJson # convert the memory Json-object to a Json-formatted memory string
+
+#### 5. save the result Json-Object string back to the SAME file overwriting it with new data set
+$updateJson | Out-File -Path $updateFile -Encoding UTF8  # save the Json-formatted string to the file overwriting it with new data
+
